10dded46a3c52f8f2357f13612a191bc8a389aec
hiroshiSxc <141240361+hiroshiSxc@users.noreply.github.com>
2024-11-20 11:05:09 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Esporte',
+    'Quem é o jogador com mais gols marcados pelo Real Madrid?',
+    'Cristiano Ronaldo(vulgo papai cris).'
+)
+
+criaCartao(
+    'Espoete',
+    'Quantos títulos da La Liga o Real Madrid conquistou até 2024?',
+    '35 título'
+)
+
+criaCartao(
+    'Esporte',
+    'Quem foi o técnico do Real Madrid que levou o time à vitória na Liga dos Campeões em 2016, 2017 e 2018?',
+    'Zinedine Zidane.'
+)
+
+criaCartao(
+    'Esporte',
+    'Qual ano foi fundado o Real Madrid?',
+    '1902'
+)
